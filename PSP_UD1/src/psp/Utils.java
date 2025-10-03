ef08c27738f77f837faef9af282e4b120b307b7d
jmmorsan <moreno.sanchez.juan.manuel@iescamas.es>
2025-10-03 20:50:09 +0200

    Creacion de Utils

--- PSP_UD1/src/psp/Utils.java
+++ PSP_UD1/src/psp/Utils.java
@@ -1,5 +1,21 @@
 package psp;
-
+//Utils.java
+import java.util.*;
 public class Utils {
-
+	
+ public static boolean isWindows() {
+     return System.getProperty("os.name").toLowerCase().contains("win");
+ }
+ 
+ 
+ 	public static List<String> sh(String cmd) {
+ 		return isWindows() ? Arrays.asList("cmd","/c",cmd)
+                        	: Arrays.asList("sh","-c",cmd);
+ 	}
+ 	
+ 	/*
+ 	public static void main(String [] args) {
+ 		System.out.println(System.getProperty("os.name"));
+ 	}*/
+ 
 }
