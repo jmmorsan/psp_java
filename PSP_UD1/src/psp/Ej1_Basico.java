dd1f32f971457459cce7c9fca379f97b62654d28
jmmorsan <moreno.sanchez.juan.manuel@iescamas.es>
2025-10-03 21:00:16 +0200

    Creacion de Ej1_Basico

---
+++ PSP_UD1/src/psp/Ej1_Basico.java
@@ -0,0 +1,11 @@
+package psp;
+
+import java.util.*;
+
+public class Ej1_Basico {
+	public static void main (String [] args) throws Exception {
+		Process p = new ProcessBuilder(Utils.sh("echo hola")).start();
+		int rc = p.waitFor();
+		System.out.println("RC=" + rc);
+	}
+}
